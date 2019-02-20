0835c84fe9e7ea3abbd1caea64eb8a3bfe9cab18
Ivan Potapov <ingumsky@gmail.com>
2019-02-20 13:33:58 +0300

    Minor stylistic changes to index.js (linting).

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 /*
-	Go to previous/next "page"
+	go to previous/next 'page'
 	@isNext - Boolean value
-	@rows 	- Number of rows in a "page" to scroll
+	@rows 	- Number of rows in a 'page' to scroll
 */
 let gotoPage = (isNext, rows) => {
-	const 	direction = isNext ? "next" : "prev";
+	const 	direction = isNext ? 'next' : 'prev';
 	const 	buttons = {
 		prev: document.getElementById('decrementPage') || document.getElementsByClassName('vertical-scrollbar-up-button scrollbar-button-enabled')[0], // get a previous page button from either old or new interface
 		next: document.getElementById('incrementPage') || document.getElementsByClassName('vertical-scrollbar-down-button scrollbar-button-enabled')[0], // get a next page button handler from either old or new interface
@@ -17,41 +17,41 @@ let gotoPage = (isNext, rows) => {
 		node.dispatchEvent(clickEvent);
 	};
 	if (buttons[direction]) {
-		// The action does the job for "proper pages" (old interface)
+		// the action does the job for 'proper pages' (old interface)
 		quasiClick(buttons[direction], 'click');
-		// The action does the job for "infinite pages" (new interface)
+		// the action does the job for 'infinite pages' (new interface)
 		if (!rows) { rows = 20; }
 		for (let i = 0; i < rows; i++) {
 			quasiClick(buttons[direction], 'mousedown');
 			quasiClick(buttons[direction], 'mouseup');
-		};
+		}
 	} else {
-		console.log("Sorry, no page to go");
+		console.log('Sorry, no page to go');
 	}
-}
+};
 
 /*
-	Go to the next segment missing translation or not confirmed yet
+	go to the next segment missing translation or not confirmed yet
 	@isNext - Boolean value
 */
 let gotoEmpty = (isNext) => {
-	let editable = $(".status-cell:not(.confirmed):not(.r1confirmed):not(.r2confirmed):not(:has(a.locked))").prev(".translated-segment-grid").parent("tr:not(.lock):not(.not-loaded-row)");
+	let editable = $('.status-cell:not(.confirmed):not(.r1confirmed):not(.r2confirmed):not(:has(a.locked))').prev('.translated-segment-grid').parent('tr:not(.lock):not(.not-loaded-row)');
 	if (!editable.length) {
 		gotoPage(isNext);
 		setTimeout(() => {memoq.nextEmpty()}, 400);
 	} else {
 		editable[0].scrollIntoView(true);
-		console.log("Found a result:", editable[0]);
+		console.log('Found a result:', editable[0]);
 		// editable[0].click();
 	}
 };
 
-/* 
-	Use one of the TM suggestions
+/*
+	use one of the TM suggestions
 	@choice - row number to choose from a TM suggestion list
 */
 let useTm = (choice) => {
-	let suggestions = $("#resultsList tr");
+	let suggestions = $('#resultsList tr');
 	if (!suggestions.length || !choice || isNaN(choice)) { return; } // no TM suggestions or choice is invalid
 	choice = !choice ? 0 : Number(choice) - 1; // use the first suggestion available on the list
 	$(suggestions[choice]).dblclick();
@@ -59,11 +59,11 @@ let useTm = (choice) => {
 
 
 /*
-	Gets length of the segment currently edited
+	gets length of the segment currently edited
 	Returns the number
 */
 let getLength = () => {
-	let ed =  $(".focused .editor-char")[0];
+	let ed =  $('.focused .editor-char')[0];
 	if (ed) { // segment is being edited
 		return ed.innerText.length;
 	}
